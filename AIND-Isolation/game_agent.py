de01993b0bf0668f2ada32fc1763bcaf52133d5c
Yao Wu <yaoyaowu@yahoo.com>
2018-03-29 10:57:35 -0400

    term1

---
+++ AIND-Isolation/game_agent.py
@@ -0,0 +1,331 @@
+"""Finish all TODO items in this file to complete the isolation project, then
+test your agent's strength against a set of known agents using tournament.py
+and include the results in your report.
+"""
+import math
+
+class SearchTimeout(Exception):
+    """Subclass base exception for code clarity. """
+    pass
+
+
+def custom_score(game, player):
+    """Calculate the heuristic value of a game state from the point of view
+    of the given player.
+
+    Penalize player moves that allow opponent to have more wiggle room.
+    """
+    if game.is_winner(player):
+        return float("inf")
+    if game.is_loser(player):
+        return float("-inf")
+
+    player_moves = game.get_legal_moves(player)
+    opp_moves = game.get_legal_moves(game.get_opponent(player))
+
+    return float(len(player_moves)**2 - (len(opp_moves)**2))
+
+def custom_score_2(game, player):
+    """Calculate the heuristic value of a game state from the point of view
+    of the given player.
+
+    Gradually increase the penalty for the number of opponent's moves as the board gets more crowded. 
+    """
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    player_moves = len(game.get_legal_moves(player))
+    opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
+
+    blank_pct = float(len(game.get_blank_spaces())) / (game.width * game.height)
+    decay = 1 + min(1/blank_pct, 20)/10
+
+    return float(player_moves - decay * opp_moves)
+
+
+def custom_score_3(game, player):
+    """Calculate the heuristic value of a game state from the point of view
+    of the given player.
+
+    Reward player moves that are closer to the center of the board thus limit the opponent's options
+    """
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    player_moves = game.get_legal_moves(player)
+    opp = game.get_opponent(player)
+    opp_moves = game.get_legal_moves(opp)
+
+    difference_in_moves = len(player_moves) - len(opp_moves)
+    player_pos = game.get_player_location(player)
+    opp_pos = game.get_player_location(opp)
+
+    w, h = game.width / 2., game.height / 2.
+
+    distance_player = (player_pos[0]-h)**2 + (player_pos[1]-w)**2
+    distance_opp = (opp_pos[0]-h)**2 + (opp_pos[1]-w)**2
+
+    return float(difference_in_moves - math.sqrt(distance_player) + math.sqrt(distance_opp))
+
+
+class IsolationPlayer:
+    """Base class for minimax and alphabeta agents -- this class is never
+    constructed or tested directly.
+
+    ********************  DO NOT MODIFY THIS CLASS  ********************
+
+    Parameters
+    ----------
+    search_depth : int (optional)
+        A strictly positive integer (i.e., 1, 2, 3,...) for the number of
+        layers in the game tree to explore for fixed-depth search. (i.e., a
+        depth of one (1) would only explore the immediate sucessors of the
+        current state.)
+
+    score_fn : callable (optional)
+        A function to use for heuristic evaluation of game states.
+
+    timeout : float (optional)
+        Time remaining (in milliseconds) when search is aborted. Should be a
+        positive value large enough to allow the function to return before the
+        timer expires.
+    """
+    def __init__(self, search_depth=3, score_fn=custom_score, timeout=10.):
+        self.search_depth = search_depth
+        self.score = score_fn
+        self.time_left = None
+        self.TIMER_THRESHOLD = timeout
+
+
+class MinimaxPlayer(IsolationPlayer):
+    """Game-playing agent that chooses a move using depth-limited minimax
+    search. You must finish and test this player to make sure it properly uses
+    minimax to return a good move before the search time limit expires.
+    """
+
+    def get_move(self, game, time_left):
+        """Search for the best move from the available legal moves and return a
+        result before the time limit expires.
+
+        **************  YOU DO NOT NEED TO MODIFY THIS FUNCTION  *************
+
+        For fixed-depth search, this function simply wraps the call to the
+        minimax method, but this method provides a common interface for all
+        Isolation agents, and you will replace it in the AlphaBetaPlayer with
+        iterative deepening search.
+
+        Parameters
+        ----------
+        game : `isolation.Board`
+            An instance of `isolation.Board` encoding the current state of the
+            game (e.g., player locations and blocked cells).
+
+        time_left : callable
+            A function that returns the number of milliseconds left in the
+            current turn. Returning with any less than 0 ms remaining forfeits
+            the game.
+
+        Returns
+        -------
+        (int, int)
+            Board coordinates corresponding to a legal move; may return
+            (-1, -1) if there are no available legal moves.
+        """
+        self.time_left = time_left
+
+        # Initialize the best move so that this function returns something
+        # in case the search fails due to timeout
+        best_move = (-1, -1)
+
+        try:
+            # The try/except block will automatically catch the exception
+            # raised when the timer is about to expire.
+            return self.minimax(game, self.search_depth)
+
+        except SearchTimeout:
+            pass  # Handle any actions required after timeout as needed
+
+        # Return the best move from the last completed search iteration
+        return best_move
+
+    def terminal_test(self, game, depth):
+        """ Return True if the game is over for the active player
+        and False otherwise.
+        """
+        return (not len(game.get_legal_moves(game.active_player))) or depth == 0
+
+    def minimax(self, game, depth):
+        """Implement depth-limited minimax search algorithm as described in
+        the lectures.
+
+        This should be a modified version of MINIMAX-DECISION in the AIMA text.
+        https://github.com/aimacode/aima-pseudocode/blob/master/md/Minimax-Decision.md
+
+        **********************************************************************
+            You MAY add additional methods to this class, or define helper
+                 functions to implement the required functionality.
+        **********************************************************************
+
+        Parameters
+        ----------
+        game : isolation.Board
+            An instance of the Isolation game `Board` class representing the
+            current game state
+
+        depth : int
+            Depth is an integer representing the maximum number of plies to
+            search in the game tree before aborting
+
+        Returns
+        -------
+        (int, int)
+            The board coordinates of the best move found in the current search;
+            (-1, -1) if there are no legal moves
+
+        Notes
+        -----
+            (1) You MUST use the `self.score()` method for board evaluation
+                to pass the project tests; you cannot call any other evaluation
+                function directly.
+
+            (2) If you use any helper functions (e.g., as shown in the AIMA
+                pseudocode) then you must copy the timer check into the top of
+                each helper function or else your agent will timeout during
+                testing.
+        """
+        if self.time_left() < self.TIMER_THRESHOLD:
+            raise SearchTimeout()
+
+        if self.terminal_test(game, depth):
+            return None
+
+        best_f, best_move, best_v = self.best_init(game)
+
+        for move in game.get_legal_moves():
+            forecast = game.forecast_move(move)
+            new_v = self.minimax_v(forecast, depth - 1)
+            if best_f(new_v, best_v) == new_v:
+                best_v = new_v
+                best_move = move
+        return best_move
+
+    def minimax_v(self, game, depth):
+        """
+            Return the best score for minimax 
+        """
+        if self.time_left() < self.TIMER_THRESHOLD:
+            raise SearchTimeout()
+
+        if self.terminal_test(game, depth):
+            return self.score(game, self)
+
+        best_f, _, best_v = self.best_init(game)
+
+        for move in game.get_legal_moves():
+            forecast = game.forecast_move(move)
+            new_v = self.minimax_v(forecast, depth - 1)
+            if best_f(new_v, best_v) == new_v:
+                best_v = new_v
+        return best_v
+
+    def best_init(self, game):
+        """
+            Initialize search
+        """
+        best_move = (-1, -1)
+
+        if game.active_player == self:
+            best_f = max
+            best_v = float('-inf')
+        else:
+            best_f = min
+            best_v = float('inf')
+        return best_f, best_move, best_v
+
+
+class AlphaBetaPlayer(IsolationPlayer):
+    """Game-playing agent that chooses a move using iterative deepening minimax
+    search with alpha-beta pruning. You must finish and test this player to
+    make sure it returns a good move before the search time limit expires.
+    """
+
+    def get_move(self, game, time_left):
+        """Search for the best move from the available legal moves and return a
+        result before the time limit expires.
+        """
+        self.time_left = time_left
+
+
+        # Initialize the best move so that this function returns something
+        # in case the search fails due to timeout
+        best_move = (-1, -1)
+        depth = 1
+
+        while True:
+            try:
+                # The try/except block will automatically catch the exception
+                # raised when the timer is about to expire.
+                best_move = self.alphabeta(game, depth)
+                depth += 1
+
+            except SearchTimeout:
+                break  # Handle any actions required after timeout as needed
+
+        # Return the best move from the last completed search iteration
+        return best_move
+
+    def alphabeta(self, game, depth, alpha=float("-inf"), beta=float("inf")):
+        """Implement depth-limited minimax search with alpha-beta pruning as
+        described in the lectures.
+        """
+        if self.time_left() < self.TIMER_THRESHOLD:
+            raise SearchTimeout()
+
+        return self.min_max_value(game, depth, alpha, beta)[0]
+
+    def min_max_value(self, game, depth, alpha, beta, maximizing=True):
+        """ 
+        Implement alpha beta pruning.
+        """
+        if self.time_left() < self.TIMER_THRESHOLD:
+            raise SearchTimeout()
+
+        moves = game.get_legal_moves()
+        if not len(moves):
+            return (-1, -1), self.score(game, self)
+
+        if depth == 0:
+            return moves[0], self.score(game, self)
+
+        move_and_score = list()
+        for move in moves:
+            _, score = self.min_max_value(game.forecast_move(move),
+                                      depth - 1,
+                                      alpha,
+                                      beta,
+                                      not maximizing)
+
+            move_and_score.append((move, score))
+
+            if maximizing:
+                alpha = max(alpha, score)
+            else:
+                beta = min(beta, score)
+
+            if beta <= alpha:
+                break
+
+        if maximizing:
+            final_score = max(move_and_score, key=lambda x: x[1])
+        else:
+            final_score = min(move_and_score, key=lambda x: x[1])
+
+        return final_score
+
+
---
+++ AIND-Sudoku/README.md
@@ -0,0 +1,46 @@
+# Artificial Intelligence Nanodegree
+## Introductory Project: Diagonal Sudoku Solver
+
+# Question 1 (Naked Twins)
+Q: How do we use constraint propagation to solve the naked twins problem?  
+A: 
+    1. Find all naked twins. Naked twins have to live in the same unit, they have only two possbile values, and these values have to be the same. Once these three criteria is met, we can classify the pair of boxes as naked twins. 
+    2. Eliminate the values of naked twins from the search space of their peers. We have to be careful to keep the values for the naked twins, otherwise, one of the naked twins' values would be empty.
+
+# Question 2 (Diagonal Sudoku)
+Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
+A: In addition to the row/column/square constaints. The boxes on the two diagonals should only have 1 to 9 exactly once. So we need to add the diagonal constraint to units to limit search space. 
+
+### Install
+
+This project requires **Python 3**.
+
+We recommend students install [Anaconda](https://www.continuum.io/downloads), a pre-packaged Python distribution that contains all of the necessary libraries and software for this project. 
+Please try using the environment we provided in the Anaconda lesson of the Nanodegree.
+
+##### Optional: Pygame
+
+Optionally, you can also install pygame if you want to see your visualization. If you've followed our instructions for setting up our conda environment, you should be all set.
+
+If not, please see how to download pygame [here](http://www.pygame.org/download.shtml).
+
+### Code
+
+* `solution.py` - Fill in the required functions in this file to complete the project.
+* `test_solution.py` - You can test your solution by running `python -m unittest`.
+* `PySudoku.py` - This is code for visualizing your solution.
+* `visualize.py` - This is code for visualizing your solution.
+
+### Visualizing
+
+To visualize your solution, please only assign values to the values_dict using the `assign_value` function provided in solution.py
+
+### Submission
+Before submitting your solution to a reviewer, you are required to submit your project to Udacity's Project Assistant, which will provide some initial feedback.  
+
+The setup is simple.  If you have not installed the client tool already, then you may do so with the command `pip install udacity-pa`.  
+
+To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login) for alternate login instructions.
+
+This process will create a zipfile in your top-level directory named sudoku-<id>.zip.  This is the file that you should submit to the Udacity reviews system.
+
